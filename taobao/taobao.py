15bc1b41dabec9e415035819ba3279520bac456f
tiandiduwu <liuzeyunqz@163.com>
2017-12-28 00:04:58 +0800

    spider wiki link

--- taobao/taobao.py
+++ taobao/taobao.py
@@ -1,1 +1,33 @@
+import io
+import re
+import sys
+import webbrowser
+from urllib import request
+from bs4 import BeautifulSoup
 
+url = 'https://en.wikipedia.org/wiki/Kevin_Bacon'
+def getSoup(url):
+    req = request.Request(url)
+    #设置请求头
+    req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36')
+    try:
+        response = request.urlopen(req)
+        return BeautifulSoup(response.read(), "html.parser")
+    except AttributeError as e:
+        print(e)
+    except HTTPError as e:
+        print(e)
+
+soup = getSoup(url)
+
+for link in soup.find_all("a"):
+    f = open('wikiLink.txt',"a+")
+    if 'href' in link.attrs:
+        f.write(link.attrs['href']+"\n")
+    f.close()
+
+for link in soup.find("div", {"id":"bodyContent"}).findAll("a",href=re.compile("^(/wiki/)((?!:).)*$")):
+    f = open('wikiLess.txt',"a+")
+    if 'href' in link.attrs:
+        f.write(link.attrs['href']+"\n")
+    f.close()
